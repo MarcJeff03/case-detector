5202e65c1213d86da70003674c52c8151a7b8626
Marc Jeff Gabasa <gabasamarcjeff03@gmail.com>
2025-11-24 10:02:30 +0800

    Fix middleware order for proper CSRF handling

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -51,10 +51,10 @@ INSTALLED_APPS = [
 # https://docs.djangoproject.com/en/2.1/topics/http/middleware/
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
